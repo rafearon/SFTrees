f275e28cc474252a70fe0ca3453213d9b3e8b5ee
grace-young <graciewhy@gmail.com>
2017-10-22 19:09:18 -0700

    add svg dashboard and almost a button

--- data/trees.js
+++ data/trees.js
@@ -6,16 +6,20 @@
 var mapWidth = 750;
 var mapHeight = 750;
 
+var mapDashboardHorzPadding = 20;
+
 // Set up projection that the map is using
 var projection = d3.geoMercator()
-  .center([-122.433701, 37.767683]) // San Francisco, roughly
-  .scale(225000)
-  .translate([mapWidth / 2, mapHeight / 2]);
+    .center([-122.433701, 37.767683]) // San Francisco, roughly
+    .scale(225000)
+    .translate([mapWidth / 2, mapHeight / 2]);
 
 // This is the mapping between <longitude, latitude> position to <x, y> pixel position on the map
 // projection is a function and it has an inverse:
 // projection([lon, lat]) returns [x, y]
 // projection.invert([x, y]) returns [lon, lat]
+
+
 
 // Add an SVG element to the DOM
 var svg = d3.select('body').append('svg')
@@ -23,13 +27,30 @@ var svg = d3.select('body').append('svg')
   .attr('height', mapHeight)
   .attr('id', 'sf-map');
 
-let plot = d3.select('#sf-map').append('g')
+var dashboardSvg = d3.select('body').append('svg')
+  .attr('width', 200 + mapDashboardHorzPadding)
+  .attr('height', mapHeight)
+  .attr('x', mapWidth + 10)
+  .attr('y', -200)
+  .attr('id', 'dashboard');
+
+let plot = d3.select('#sf-map').append('g');
+
 
 // Add SVG map at correct size, assuming map is saved in a subdirectory called `data`
 svg.append('image')
   .attr('width', mapWidth)
-  .attr('height', mapHeight)
+  .attr('height', mapHeight- 100)
   .attr('xlink:href', 'data/sf-map.svg');
+
+
+// Set up Dashboard elements
+var dashboardData = [{name:"Select Points", selected:false}]
+/*dashboardSvg.append('rect')
+  .attr('width', 50)
+  .attr('height', 25)
+  .attr('fill', 'grey');
+*/
 
 
 d3.csv('trees.csv', parseInputRow, loadData);
@@ -41,8 +62,8 @@ d3.csv('trees.csv', parseInputRow, loadData);
 function loadData(error, treeData){
 	if(error) throw error;
 	drawTreeMap(treeData);
-	console.log(treeData)
-
+    drawDashboard();
+	console.log(treeData);
 }
 
 function drawTreeMap(treeData) {
@@ -54,15 +75,61 @@ function drawTreeMap(treeData) {
 	.attr('cx', function(d) {return projection([d.longitude, d.latitude])[0];}) 
 	.attr('cy', function(d) {return projection([d.longitude, d.latitude])[1];})
 	.style('fill', 'blue');
-	let unselectedCircles = updateCircles.exit();
+	let unselectedCircles = updatedCircles.exit();
 	updatedCircles.exit().remove(); 
-
-
-
-
 }
 
+function drawDashboard(){
+    /*
+    StackOverflow post that helped find how to append text to SVG element
+    https://stackoverflow.com/questions/20644415/d3-appending-text-to-a-svg-rectangle
+    
+    
+    var elems = dashboardElem.selectAll("g")
+        .data(dashboardData)
+        .enter().append("g");
+//        .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
 
+    elems.append("rect")
+        .attr("width", 25)
+        .attr("height", 15)
+        .attr("fill","orange");
+*/
+    var elems = dashboardSvg.selectAll('g')
+        .data(dashboardData)
+        .enter()
+        .append('g');
+    
+    elems.append('rect')
+        .attr('width', 50)
+        .attr('height', 25)
+        .attr('fill', 'grey');
+        
+    elems.append('text')
+        .text("test")
+        .attr('x', 5)
+        .attr('y', 10)
+        .attr('width', 100)
+        .attr('height', 100)
+        .attr('fill', 'black');
+    
+
+    
+/*    elems.append("text")
+        .attr("x", function(d) { return x(d) - 3; })
+        .attr("y", barHeight / 2)
+        .attr("dy", ".35em")
+        .text(function(d) { return d; });
+    
+    
+    
+    let elems = dashboardElem.selectAll('rect')
+        .data(dashboardData)
+        .enter()
+        .append('rect')
+        .fillText("meep");
+  */      
+}
 
 function parseInputRow (d) {
         //console.log(d)
